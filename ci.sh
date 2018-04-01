51b1d5eee0612596610c20ecc75e4582404663af
Lars Krueger <lars_e_krueger@gmx.de>
2018-04-01 17:09:57 +0200

    non-functional: Only show first errors in ci.sh

--- ci.sh
+++ ci.sh
@@ -1,8 +1,11 @@
-#! /bin/sh
+#! /bin/bash
+
+LINES=$(tput lines)
+((LINES=$LINES/2))
 
 inotifywait -m -e close_write $(find src -type d) | while read line; do
   if echo $line | grep '\.rs$'; then
      clear
-     cargo test
+     cargo test |& head -n $LINES
   fi
 done
