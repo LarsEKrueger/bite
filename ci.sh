986f0c6656e32f618ab96f378487cd2789c36138
Lars Krueger <lars_e_krueger@gmx.de>
2018-05-21 20:43:55 +0200

    non-functional: ci script also watch c_src
    to recompile interfaces

--- ci.sh
+++ ci.sh
@@ -3,7 +3,7 @@
 LINES=$(tput lines)
 ((LINES=$LINES/2))
 
-inotifywait -m -e close_write $(find src -type d) | while read line; do
+inotifywait -m -e close_write $(find src -type d) c_src | while read line; do
   if echo $line | grep '\.rs$'; then
      clear
      cargo test |& head -n $LINES
