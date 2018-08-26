fe515b2454df26c8d67f94992448c4ba6a648329
Lars Krueger <lars_e_krueger@gmx.de>
2018-08-26 15:18:49 +0200

    non-functional: Improve display of ci script

--- ci.sh
+++ ci.sh
@@ -6,6 +6,9 @@ LINES=$(tput lines)
 inotifywait -m -e close_write $(find src -type d) c_src | while read line; do
   if echo $line | grep '\.rs$'; then
      clear
-     (cargo build --color always; cargo test --color always) |& head -n $LINES
+     cargo build --color always
+     if [ $? == 0 ] ; then
+       cargo test --color always | grep -v '^test .*ok$'
+     fi
   fi
 done
