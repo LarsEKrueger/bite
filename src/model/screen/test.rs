37680470a59b72e98cc9d8019a8630a318dbf9d1
Lars Krueger <lars_e_krueger@gmx.de>
2020-03-28 17:26:31 +0100

    style: Formatting

--- src/model/screen/test.rs
+++ src/model/screen/test.rs
@@ -920,10 +920,7 @@ fn word_before_cursor() {
 
 #[test]
 fn erase_character_at_eol() {
-  Test::e(b"Hello\x1b[10X")
-      .cp(5,0)
-      .width(16)
-      .height(1);
+    Test::e(b"Hello\x1b[10X").cp(5, 0).width(16).height(1);
 }
 
 // TODO: Test for protected
--- src/presenter/tui.rs
+++ src/presenter/tui.rs
@@ -51,13 +51,19 @@ impl TuiExecuteCommandPresenter {
     }
 
     fn send_string(&mut self, send: &str) -> PresenterCommand {
-        self.commons.interpreter.jobs.write_stdin(self.current_interaction, send.as_bytes());
+        self.commons
+            .interpreter
+            .jobs
+            .write_stdin(self.current_interaction, send.as_bytes());
         PresenterCommand::Redraw
     }
 
     fn send_term_info(&mut self, cap_name: &str) -> PresenterCommand {
         if let Some(cap_str) = self.commons.term_info.strings.get(cap_name) {
-            self.commons.interpreter.jobs.write_stdin(self.current_interaction, cap_str);
+            self.commons
+                .interpreter
+                .jobs
+                .write_stdin(self.current_interaction, cap_str);
             PresenterCommand::Redraw
         } else {
             PresenterCommand::Unknown
